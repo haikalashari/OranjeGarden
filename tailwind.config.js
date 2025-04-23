e592cbe0995f2d910dbecc4893f0450b80d72160
haikalashari <haikalashari11@gmail.com>
2025-04-23 09:48:30 +0700

    tailwind install works

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class', // ← enable class-based dark mode
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 
     theme: {
