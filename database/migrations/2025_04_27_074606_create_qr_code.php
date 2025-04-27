6a070dfbb08bfca80f345d9dc3b7589ba5e0f8d5
haikalashari <haikalashari11@gmail.com>
2025-04-27 14:56:26 +0700

    add file

---
+++ database/migrations/2025_04_27_074606_create_qr_code.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('qr_code', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->string('qr_code_delivered')->nullable();
+            $table->string('qr_code_returned')->nullable();
+            $table->timestamp('qr_code_delivered_used_at')->nullable();
+            $table->timestamp('qr_code_returned_used_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('qr_code');
+    }
+};
